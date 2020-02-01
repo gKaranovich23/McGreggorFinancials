dd7849d7729a5f7f973f5084d0084b6876be2e01
Gregg Logan Karanovich <gkaranovich3@gmail.com>
2020-02-01 17:10:49 -0500

    added removeAccountDescription Migration

--- McGreggorFinancials/McGreggorFinancials/Migrations/20200201220918_RemovedAccountDescription.Designer.cs
+++ McGreggorFinancials/McGreggorFinancials/Migrations/20200201220918_RemovedAccountDescription.Designer.cs
@@ -4,14 +4,16 @@ using McGreggorFinancials.Models.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace McGreggorFinancials.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200201220918_RemovedAccountDescription")]
+    partial class RemovedAccountDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
